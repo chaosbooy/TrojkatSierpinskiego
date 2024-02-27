f5bf90e10b7f126a6de4226448c5fa228d007099
chaosbooy <lpmelon234@gmail.com>
2024-02-27 23:12:38 +0100

    Initial commit

---
+++ TrojkatSierpinskiego/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace TrojkatSierpinskiego
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SufferSize = new System.Windows.Forms.NumericUpDown();
+            this.Generate = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.SufferSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SufferSize
+            // 
+            this.SufferSize.BackColor = System.Drawing.SystemColors.Window;
+            this.SufferSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.SufferSize.Location = new System.Drawing.Point(109, 12);
+            this.SufferSize.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.SufferSize.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.SufferSize.Name = "SufferSize";
+            this.SufferSize.Size = new System.Drawing.Size(120, 38);
+            this.SufferSize.TabIndex = 1;
+            this.SufferSize.Value = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            // 
+            // Generate
+            // 
+            this.Generate.Location = new System.Drawing.Point(235, 12);
+            this.Generate.Name = "Generate";
+            this.Generate.Size = new System.Drawing.Size(161, 38);
+            this.Generate.TabIndex = 2;
+            this.Generate.Text = "Generate suffering";
+            this.Generate.UseVisualStyleBackColor = true;
+            this.Generate.Click += new System.EventHandler(this.Suffer);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(402, 12);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(49, 38);
+            this.Clear.TabIndex = 3;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.BackColor = System.Drawing.SystemColors.Window;
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.numericUpDown1.Location = new System.Drawing.Point(32, 12);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(71, 38);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(120, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Triangle Size";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(41, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Layers";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Generate);
+            this.Controls.Add(this.SufferSize);
+            this.Name = "Form1";
+            this.Text = "Trójkąt Cierpi - ńskiego";
+            ((System.ComponentModel.ISupportInitialize)(this.SufferSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown SufferSize;
+        private System.Windows.Forms.Button Generate;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TrojkatSierpinskiego/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TrojkatSierpinskiego
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// variables needed for the program to work: number of triangle layers, offset from application bounds 
+        /// </summary>
+        int layers;
+        int offset;
+        SolidBrush whiteBrush;
+        Graphics g;
+
+        public Form1()
+        {
+            InitializeComponent();
+            layers = 0;
+            offset = 80;
+            whiteBrush = new SolidBrush(Color.White);
+        }
+
+        /// <summary>
+        /// Starts the whole process of drawing
+        /// </summary>
+        /// <remarks>
+        /// based on numericUpDown1 having the number of white triangles and SufferSize having the length of one 
+        /// side of the triangle drawing a big black triangle first for the background and then the white 
+        /// triangles 3^[layers] times where each layer is 4 times smaller than the other
+        /// </remarks>
+        private void Suffer(object sender, EventArgs e)
+        {
+            //reloads the objects so it has a good enough size and clears it from everything
+            g = this.CreateGraphics();
+            g.Clear(BackColor);
+            if (Controls.ContainsKey("Hi"))
+                Controls.RemoveByKey("Hi");
+
+            //gets the required information for the suffering triangles
+            layers = Convert.ToInt32(numericUpDown1.Value);
+            int height = EstimateHeight(Convert.ToInt32(SufferSize.Value));
+            int width = Convert.ToInt32(SufferSize.Value);
+            
+            //Create the big black triangle
+            Point[] points = new Point[3];
+            points[0] = new Point(width / 2, offset);
+            points[1] = new Point(width, height + offset);
+            points[2] = new Point(0,height + offset);
+            SolidBrush blackBrush = new SolidBrush(Color.Black);
+            g.FillPolygon(blackBrush, points);
+
+            //Create the white triangles [layers] times
+            if (layers == 0) return;
+            CreateWhiteOnes(width, height, new Point(), 0);
+
+            if (width == 1000 && layers == 10)
+            {
+                Label Hi = new Label()
+                {
+                    Text = "Hello World",
+                    Font = new Font("Microsoft Sans Serif", 20f),
+                    Location = new Point(430, 600),
+                    AutoSize = true,
+                    TabIndex = 10,
+                    BackColor = Color.White,
+                    Name = "Hi",
+                };
+
+                Controls.Add(Hi);
+            }
+        }
+
+        /// <summary>
+        /// drawing white triangles in recursion
+        /// </summary>
+        /// <param name="width">width of the drawn triangle</param>
+        /// <param name="height">height of the drawn triangle</param>
+        /// <param name="start"> the most right-bottom point</param>
+        /// <param name="it"> number of iteration</param>
+        /// <remarks>
+        /// draws an upside down white triangle in the center of the black triangle in its background
+        /// then if it doesnt go beyond the iteration it creates its clones it recreates itself 3 times while positioning the clones so that
+        /// they have their own black triangle background
+        /// </remarks>
+        private void CreateWhiteOnes(int width, int height, Point start, int it)
+        {
+            int quaterWidth = width / 4;
+            int halfHeight = height / 2;
+
+            //the points of the drawn triangle calculated first 1/4 width, 1/2 height
+            //second 3/4 width, 1/2 height and third 1/2 width and full height (everything + offsets)
+            Point[] points = new Point[3] {
+                new Point(quaterWidth + start.X, halfHeight + offset + start.Y),
+                new Point(width - quaterWidth + start.X, halfHeight + offset + start.Y),
+                new Point(width / 2 + start.X, height + offset + start.Y)
+                };
+            g.FillPolygon(whiteBrush, points);
+
+            //checking if it didnt pass the limit if not continue
+            if (++it == layers || height < 7) return;
+
+            CreateWhiteOnes(width / 2, halfHeight, new Point(start.X + quaterWidth, start.Y), it);
+            CreateWhiteOnes(width / 2, halfHeight, new Point(start.X, start.Y + halfHeight), it);
+            CreateWhiteOnes(width / 2, halfHeight, new Point(start.X + width / 2, start.Y + halfHeight), it);
+        }
+
+        //clears the board
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            g = this.CreateGraphics();
+            g.Clear(BackColor);
+
+            if (Controls.ContainsKey("Hi"))
+                Controls.RemoveByKey("Hi");
+        }
+
+        //estimate height for the equilateral triangle
+        private int EstimateHeight(int side)
+        {
+            return Convert.ToInt32(side * Math.Sqrt(3) / 2);
+        }
+    }
+}
